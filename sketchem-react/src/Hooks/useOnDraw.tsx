f308855f5605f102620ab0e306a60d1ec36aa018
yana-sidanych <yana.sid150@gmail.com>
2022-03-07 22:55:43 +0200

    added mouse tracking capabilities - can detect a click location - and movement cordinations when mouse is pressed down

--- sketchem-react/src/Hooks/useOnDraw.tsx
+++ sketchem-react/src/Hooks/useOnDraw.tsx
@@ -1,18 +1,47 @@
-import { Context, useRef } from 'react'
+import { useRef } from 'react'
 import Raphael from 'raphael'
 
 function useOnDraw() {
     //ref for the canvas dom element
     const divRef = useRef<HTMLDivElement>(null!)
-    var isMousePressedRef = false
+    var isMousePressed = false
     var paper: any = null
-    var shape: any = null
 
     function setdivRef(ref: any) {
         if (!ref) return
         divRef.current = ref
-
+        setMouseDownListener()
+        setMouseUpListener()
+        setMouseMoveListener()
         paper = Raphael(divRef.current, 1500, 1000)
+    }
+
+    function setMouseDownListener() {
+        if (!divRef.current) return
+        const downListener = (e: MouseEvent) => {
+            console.log('mouse down')
+            isMousePressed = true
+            console.log({ x: e.screenX, y: e.screenY })
+        }
+        divRef.current.addEventListener('mousedown', downListener)
+    }
+
+    function setMouseUpListener() {
+        if (!divRef.current) return
+        const upListener = (e: MouseEvent) => {
+            console.log('mouse up')
+            isMousePressed = false
+        }
+        window.addEventListener('mouseup', upListener)
+    }
+
+    function setMouseMoveListener() {
+        const MoveListener = (e: MouseEvent) => {
+            if (!isMousePressed) return
+            console.log({ x: e.screenX, y: e.screenY })
+        }
+
+        divRef.current.addEventListener('mousemove', MoveListener)
     }
 
     return setdivRef
